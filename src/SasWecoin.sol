2cdfaa5646c2e01b3c5bfe17ac34cdba699f9b61
Semi Invader <=>
2023-08-03 00:10:04 -0600

    Claim function v0.5
    Co-authored-by: Gabriel Garoz <gab.120gg@gmail.com>
    Co-authored-by: Chuz <susguzman@gmail.com>

--- src/SasWecoin.sol
+++ src/SasWecoin.sol
@@ -1,2 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
+
+import "openzeppelin/token/ERC20/IERC20.sol";
+import "./interfaces/ISasWecoin.sol";
+
+contract SasWecoin is ISasWecoin {
+    mapping(address => UserInfo) public users;
+
+    IERC20 public WECOIN;
+
+    uint public totalRewards;
+    uint public stakingStartTime;
+    uint public prevTimestamp;
+    uint public totalStakingPower;
+    uint public accumulatedRewardsPerStakingPower;
+    uint private lastAccumulatedEpoch;
+    uint private lastTotalReward;
+    uint private constant MAGNIFIER = 1e12;
+
+    function claim(address _user) external view returns (uint256) {
+        UserInfo storage user = users[_user];
+
+        //TODO - we have to know the user staking power duration.
+        //TODO - we have to know the user staking power amount.
+
+        uint lastAction = prevTimestamp;
+        uint epochCounter = (prevTimestamp - stakingStartTime) / 7 days;
+        uint currentEpoch = (block.timestamp - stakingStartTime) / 7 days;
+
+        uint diff0 = (epochCounter + 1) * 7 days + stakingStartTime;
+        diff0 -= user.lastAction;
+
+        uint rewardOffset = lastTotalReward;
+        uint totalEmitted = 0;
+        uint epochEmissions = 0;
+        for (uint i = lastAccumulatedEpoch; i <= currentEpoch; i++) {
+            epochEmissions = (rewardOffset * 28) / 100_00;
+            rewardOffset -= epochEmissions;
+            if (i == epochCounter) {
+                epochEmissions = epochEmissions / 7 days; // PER SECOND
+                diff0 =
+                    (epochEmissions * diff0 * MAGNIFIER) /
+                    totalStakingPower;
+                accumulatedRewardsPerStakingPower += diff0;
+                lastTotalReward = rewardOffset;
+            } else if (i > epochCounter && i < currentEpoch) {
+                accumulatedRewardsPerStakingPower +=
+                    (epochEmissions * MAGNIFIER) /
+                    totalStakingPower;
+            } else if (i == currentEpoch) {
+                uint diff1 = block.timestamp - (i * 7 days + stakingStartTime);
+                diff1 =
+                    (epochEmissions * diff1 * MAGNIFIER) /
+                    totalStakingPower;
+                accumulatedRewardsPerStakingPower += diff1;
+
+                lastAccumulatedEpoch = currentEpoch;
+                lastTotalReward = rewardOffset;
+            }
+        }
+
+        return
+            (user.depositAmount *
+                accumulatedRewardsPerStakingPower -
+                user.offsetPoints) / MAGNIFIER;
+    }
+}
---
+++ src/interfaces/ISasWecoin.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ISasWecoin {
+    /*-----------------------------------------------------------\
+    |                      Type Definitions                      |
+    \-----------------------------------------------------------*/
+    struct UserInfo {
+        uint depositAmount;
+        uint offsetPoints;
+        uint lastAction;
+        uint lockDuration;
+    }
+
+    /**
+     * Returns the staking power of the user, relative to the user's deposit amount and time staked.
+     * @param _user The address of the user
+     */
+    function getStakingPower(address _user) external view returns (uint256);
+
+    /**
+     * Add rewards to the total reward pool.
+     * @param _amount The amount of WECOIN to deposit
+     */
+    function addTokenRewards(uint256 _amount) external;
+
+    /**
+     * Deposit WECOIN tokens to create staking power and determine the user's share of the reward pool.
+     * @param _amount Amount of WECOIN to deposit
+     * @param _weeksLocked Time to LOCK the WECOIN tokens for
+     * @dev _weeksLocked must be saved in WEEKS, not seconds
+     * @dev penalty pool must be distributed amongst users that are currently staked with a duration
+     */
+    function deposit(uint256 _amount, uint256 _weeksLocked) external;
+}
