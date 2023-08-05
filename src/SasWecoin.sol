8af52076987d5b820d1f056b0909b2df761bca02
Semi Invader <=>
2023-08-05 02:35:49 -0600

    implement withdraw with penalties

--- src/SasWecoin.sol
+++ src/SasWecoin.sol
@@ -7,6 +7,10 @@ import "openzeppelin/utils/math/Math.sol";
 // TODO REMOVE
 import "forge-std/console.sol";
 
+error SasWecoin__NoBalanceToWithdraw();
+error SasWecoin__NothingToClaim();
+error SasWecoin__InvalidDepositAmount();
+
 contract SasWecoin is ISasWecoin {
     mapping(address => UserInfo) public users;
     mapping(uint => EpochInfo) public epochs;
@@ -19,15 +23,30 @@ contract SasWecoin is ISasWecoin {
     uint public totalBaseStakingPower;
     uint public totalBonusStakingPower;
     uint public accumulatedRewardsPerStakingPower;
+    uint public rewardPenalty = 5;
     uint private lastAccumulatedEpoch;
     uint private lastTotalReward;
-    uint private constant MAGNIFIER = 1e12;
+    // This magnifier keeps all decimals when doing integer division
+    // Not concerned about overflow since it is only used internally and
+    // WECOIN has 18 decimals + a max supply of 10B
+    // 29 decimal places total
+    // 29 + 20 = 49 decimal places total
+    // considering uint256 has a max of 77 decimal places
+    // THE MAX VALUE  where MAGNIFIER is used is
+    // (rewardOffset * WEEKLY_EMISSIONS * diff * MAGNIFIER)
+    //     29               2               10      20    <= DECIMALS USED
+    // MAX is 61 DECIMAL PLACES
+    uint private constant MAGNIFIER = 1e20;
     uint private constant EPOCH_DURATION = 1 weeks;
     uint public constant SQRT_ADJUSTMENT = 100_00;
     uint private constant DAILY_EMISSIONS = 4;
     uint private constant WEEKLY_EMISSIONS = 28;
     uint private constant EMISSIONS_BASE = 100_00;
+    uint private constant INT_PERCENTAGE_BASE = 100;
 
+    //---------------------------------------------------
+    //                  CONSTRUCTOR
+    //---------------------------------------------------
     constructor(address _wecoin, uint _stakingStartTime) {
         WECOIN = IERC20(_wecoin);
         stakingStartTime = _stakingStartTime;
@@ -35,6 +54,9 @@ contract SasWecoin is ISasWecoin {
     }
 
     function deposit(uint _amount, uint256 _weeksLocked) external {
+        if (_amount == 0) {
+            revert SasWecoin__InvalidDepositAmount();
+        }
         UserInfo storage user = users[msg.sender];
         // Lock or claim rewards
         // NOTE here is where offsetpoints are updated 1 time
@@ -86,6 +108,8 @@ contract SasWecoin is ISasWecoin {
     }
 
     function claimOrLock() external {
+        UserInfo storage user = users[msg.sender];
+        if (user.depositAmount == 0) revert SasWecoin__NothingToClaim();
         _claimAndLock(msg.sender);
     }
 
@@ -97,6 +121,66 @@ contract SasWecoin is ISasWecoin {
         WECOIN.transferFrom(msg.sender, address(this), amount);
         totalRewards += amount;
         lastTotalReward += amount;
+    }
+
+    function withdraw() external {
+        // Update all values
+        _claimAndLock(msg.sender);
+
+        UserInfo storage user = users[msg.sender];
+        if (user.depositAmount == 0) {
+            revert SasWecoin__NoBalanceToWithdraw();
+        }
+        uint currentEpoch = _currentEpoch();
+        uint withdrawableAmount = user.depositAmount;
+        uint penalty;
+        if (user.endLockEpoch > 0 && currentEpoch <= user.endLockEpoch) {
+            // Calculate and apply penalties
+            uint epochDiff = (user.endLockEpoch + 1) *
+                EPOCH_DURATION +
+                stakingStartTime -
+                block.timestamp;
+            uint threshold = (user.lockDuration * EPOCH_DURATION) / 2;
+            // Penalty of 50% threshold
+            if (epochDiff > threshold) {
+                // Principal penalty
+                penalty =
+                    (withdrawableAmount * rewardPenalty) /
+                    INT_PERCENTAGE_BASE;
+                withdrawableAmount -= penalty;
+
+                emit WithdrawPenalty(msg.sender, penalty, user.lockedRewards);
+                // Reward penalty fully penalized
+                penalty += user.lockedRewards;
+            } else {
+                // MATH penalty
+                penalty = (180000 * threshold) - (epochDiff * 1600);
+                penalty = (user.lockedRewards * penalty) / (threshold * 1000);
+                withdrawableAmount += user.lockedRewards - penalty;
+                emit WithdrawPenalty(msg.sender, 0, penalty);
+            }
+            totalRewards += penalty;
+            lastTotalReward += penalty;
+            // Adjust the next epochs adjustment since this is an early withdrawal
+            epochs[user.endLockEpoch + 1]
+                .totalBonusStakingPowerAdjustment -= user.bonusAmount;
+            totalBonusStakingPower -= user.bonusAmount;
+        }
+
+        totalBaseStakingPower -= user.depositAmount;
+        // Reset all values
+        users[msg.sender] = UserInfo({
+            depositAmount: 0,
+            bonusAmount: 0,
+            offsetPoints: 0,
+            lastAction: block.timestamp,
+            endLockEpoch: 0,
+            lockDuration: 0,
+            lockedRewards: 0
+        });
+
+        // Transfer out WECOIN
+        WECOIN.transfer(msg.sender, withdrawableAmount);
     }
 
     //-----------------------------------------------------------------------------------
@@ -143,6 +227,7 @@ contract SasWecoin is ISasWecoin {
         lastEpochTimeDiff -= prevTimestamp;
 
         for (uint i = lastAccumulatedEpoch; i <= currentEpoch; i++) {
+            EpochInfo storage epoch = epochs[i];
             epochEmissions = (rewardOffset * 28) / 100_00;
             rewardOffset -= epochEmissions;
             // NOTE Previous epoch accumulated
@@ -169,6 +254,7 @@ contract SasWecoin is ISasWecoin {
                 diff1 = (epochEmissions * diff1 * MAGNIFIER) / baseStakingPower;
                 accumulatedRewardsPerStakingPower += diff1;
             }
+            epoch.finalEpochAccumulation = accumulatedRewardsPerStakingPower;
         }
         lastAccumulatedEpoch = currentEpoch;
         lastTotalReward = rewardOffset;
@@ -197,6 +283,7 @@ contract SasWecoin is ISasWecoin {
         _updateAccumulator();
 
         UserInfo storage user = users[_user];
+        if (user.depositAmount == 0) return;
         // Check current EPOCH, if it's <= user.endLockEpoch
         // this determines which stakingPower to use for rewards.
         uint lastEpoch = _getLastEpoch(user.lastAction);
@@ -204,9 +291,9 @@ contract SasWecoin is ISasWecoin {
         uint userStakingPower = user.depositAmount;
         uint accumulatedRewards;
         uint claimableRewards;
+        bool hasLocked = user.endLockEpoch != 0;
 
-        if (currentEpoch <= user.endLockEpoch) {
-            //
+        if (currentEpoch <= user.endLockEpoch && hasLocked) {
             userStakingPower += user.bonusAmount;
             accumulatedRewards =
                 userStakingPower *
@@ -217,7 +304,9 @@ contract SasWecoin is ISasWecoin {
             user.offsetPoints = accumulatedRewards;
             emit LockReward(_user, user.lockedRewards);
         } else if (
-            currentEpoch > user.endLockEpoch && lastEpoch <= user.endLockEpoch
+            currentEpoch > user.endLockEpoch &&
+            lastEpoch <= user.endLockEpoch &&
+            hasLocked
         ) {
             // Add full rewards UP TO end of LOCK epoch
             uint endEpochAccumulated = epochs[user.endLockEpoch]
@@ -302,4 +391,16 @@ contract SasWecoin is ISasWecoin {
                 totalStakingPower -= epoch.totalBonusStakingPowerAdjustment;
         }
     }
+
+    function getCurrentEpoch() external view returns (uint) {
+        return _currentEpoch();
+    }
+
+    function getEpochFromTimestamp(
+        uint timestamp
+    ) external view returns (uint) {
+        return _getLastEpoch(timestamp);
+    }
+
+    function pendingRewards(address _user) external view returns (uint) {}
 }
